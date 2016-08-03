63a779bf5e8641bdc038b90fb88240a3b6709729
Shen Tian <biozinc@gmail.com>
2016-08-03 14:02:51 +0200

    Got brightness part of pixel test to work

--- src/learn_clj/core.clj
+++ src/learn_clj/core.clj
@@ -6,16 +6,38 @@
 (def dome
   (new Dome))
 
-(defn rgb [r g b]
-  (+ (* 256 256 r) (* 256 g) b))
+(def creep-speed 20)
+(def ramp-length 60)
 
 (def opc
   (new OPC "127.0.0.1" 7890))
 
+(def coord-order
+  (zipmap
+    (map (fn [i] (.get (.-coords dome) i)) (range 1560))
+    (range 1560)))
+
+(defn unit-interval-to-byte
+  [x]
+  (mod (int (* x 256)) 256))
+
+(defn get-brightness
+  [coord t]
+  (unit-interval-to-byte
+    (/
+      (- (get coord-order coord)
+        (* creep-speed t))
+        ramp-length)))
+
+(defn get-saturaiton)
+
 (def test-animation
   (proxy [DomeAnimation] [dome opc]
     (drawPixel [coord t]
-      (rgb (mod (int (* t 100)) 255) 0 0))))
+      (proxy-super getHsbColor
+        (mod (int (* t 100)) 255)
+        255
+        (get-brightness coord t)))))
 
 (defn millis []
   (System/currentTimeMillis))
