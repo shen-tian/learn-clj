3268306ae0b2eea7d4962abc5fe1756b9553f946
Shen Tian <biozinc@gmail.com>
2016-08-03 12:03:44 +0200

    Got basic dome code working. Yeah

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [org.processing/core "2.2.1"]
+    [me.lsdo/processing "1.0.0"]]
   :main ^:skip-aot learn-clj.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[lein-localrepo "0.5.3"]])
--- src/learn_clj/core.clj
+++ src/learn_clj/core.clj
@@ -1,7 +1,29 @@
 (ns learn-clj.core
+  (:import (processing.core PVector))
+  (:import (me.lsdo.processing OPC Dome DomeAnimation))
   (:gen-class))
+
+(def dome
+  (new Dome))
+
+(defn rgb [r g b]
+  (+ (* 256 256 r) (* 256 g) b))
+
+(def opc
+  (new OPC "127.0.0.1" 7890))
+
+(def test-animation
+  (proxy [DomeAnimation] [dome opc]
+    (drawPixel [coord t]
+      (rgb (mod (int (* t 100)) 255) 0 0))))
+
+(defn millis []
+  (System/currentTimeMillis))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (def start (millis))
+  (while true
+    (.draw test-animation
+      (/ (- (millis) start) 1000))))
