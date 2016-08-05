ba8addf9c5ae57150d500005c474487f7a4b7118
Shen Tian <biozinc@gmail.com>
2016-08-05 23:32:59 +0200

    Kaleidoscope working now

--- src/learn_clj/kaleidoscope.clj
+++ src/learn_clj/kaleidoscope.clj
@@ -7,7 +7,9 @@
              TriCoord
              LayoutUtil
              MathUtil
-             TriCoord$PanelOrientation)))
+             TriCoord$PanelOrientation
+             TriCoord$Axis
+             TriCoord$CoordType)))
 
 (def dome
   (new Dome))
@@ -20,30 +22,23 @@
   (mod (int (* x 255)) 256))
 
 (def base-panel
-  (new TriCoord (me.lsdo.processing.TriCoord$CoordType/PANEL)
+  (new TriCoord (TriCoord$CoordType/PANEL)
        0 0 -1))
 
 (defn base-pixel-color
   [coord t]
   (let [p (.getLocation dome coord)
-        p2 (LayoutUtil/Vrot p (* t (+ 0.5 (* 3 0.5 (+ (Math/cos (* 0.1213 t)) 1)))))
-        p3 (LayoutUtil/Vmult p2 (/ 1 (+ 1 (* 5 0.5 (+ (Math/cos (* 0.3025 t)) 1)))))
+        p2 (LayoutUtil/Vrot p (* t (+ 0.5 (* 3 0.5
+                                             (+ 1 (Math/cos (* 0.1213 t)))))))
+        p3 (LayoutUtil/Vmult p2 (/ 1 (+ 1 (* 5 0.5
+                                             (+ 1 (Math/cos (* 0.3025 t)))))))
         p4 (LayoutUtil/Vadd p3 (LayoutUtil/V (* 2 (Math/cos (* 0.2 t))) 0))]
-    {:hue        255
-     :saturation 255
-     :brightness (unit-interval->byte (* 0.5 (+ 1 (Math/cos (* 40 (.-x p4)))))) ;.5*(Math.cos(40*p.x)+1)
+    {:hue        (unit-interval->byte (MathUtil/fmod
+                                        (+ (.-x p4) (* 0.4081 t))
+                                        1.0))
+     :saturation (* 0.6 255)
+     :brightness (unit-interval->byte (* 0.5 (+ 1 (Math/cos (* 40 (.-x p4))))))
      }))
-;
-;int pos = MathUtil.mod(c.panel.u - c.panel.v, 3);
-;int rot = MathUtil.mod(c.panel.getOrientation() == TriCoord.PanelOrientation.A ? 2*pos : 1-2*pos, 6);
-;boolean flip = (MathUtil.mod(rot, 2) == 1);
-;TriCoord basePx = c.pixel.rotate(rot);
-;if (flip) {
-;           basePx = basePx.flip(TriCoord.Axis.U);
-;           }
-;
-;return getBasePixel(new DomeCoord(basePanel, basePx), t);
-
 
 (defn pixel-color
   [coord t]
@@ -54,7 +49,10 @@
                             (- 1 (* 2 pos)))
                           6)
         flip (= (MathUtil/mod rot 2) 1)
-        base-px (.rotate (.-pixel coord) rot)]
+        base-px (if flip
+                  (.flip (.rotate (.-pixel coord) rot)
+                         (TriCoord$Axis/U))
+                  (.rotate (.-pixel coord) rot))]
 
     (base-pixel-color
       (new DomeCoord base-panel base-px) t)))
